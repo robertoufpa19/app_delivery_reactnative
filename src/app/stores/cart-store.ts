653686bf0b4f6907b98609f86f0b7242cb2c9633
Roberto Rodrigues <robertoufpa19@gmail.com>
2024-02-27 11:51:43 -0300

    salvar informações do carrinho no dispositivo atraves do AsyncStorage

--- src/app/stores/cart-store.ts
+++ src/app/stores/cart-store.ts
@@ -1,6 +1,10 @@
 
 import {ProductProps} from "./../../utils/data/products"
+
 import {create} from "zustand"
+import {createJSONStorage, persist} from "zustand/middleware"
+
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 import * as cartInMemory from "./helpers/cart-in-memory"
 
@@ -14,7 +18,8 @@ type StateProps = {
     remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>((set) =>({
+export const useCartStore = create(
+    persist<StateProps>((set) =>({
     products:[],
 
     add: (product: ProductProps) => 
@@ -26,4 +31,13 @@ export const useCartStore = create<StateProps>((set) =>({
      set((state) => ({
         products: cartInMemory.remove(state.products, productId),
      })),
-}))
+
+    }), 
+    {
+       name:"delivery-rn:cart",
+       storage: createJSONStorage(() => AsyncStorage), 
+
+    }
+    
+  )
+)
